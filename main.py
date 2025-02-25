71544ab966e08336dc072f8cf331cd34ef366ca2
aurelius44 <kanashimi444@gmail.com>
2025-02-24 22:45:38 -0500

    B: Added shapes and partial ship motion

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+    
+    dt = 0
+    clock = pygame.time.Clock()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
